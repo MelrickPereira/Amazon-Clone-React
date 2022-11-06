4834dc8f9b7227c689c98792d2fab0b5db27f664
MelrickPereira <pereiramelrick1@gmail.com>
2022-11-06 23:25:24 +0530

    added routing to checkout page when clicked on basket logo

---
+++ src/Checkout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Checkout() {
+  return (
+    <div>
+      <div >
+        <h1>This is a checkout page</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Checkout
